2d2f2fa64bad3cde7ee47a71aef2da309d9c2c63
SummersBrian <summers.brian.cs@gmail.com>
2015-04-04 21:20:51 -0400

    initial commit

---
+++ PA2/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Animator animator; //animator component
+	public float vert; //vertical movement
+	public float horiz; //horizontal movement
+	public float sprint;
+
+	private bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		vert = 0.0f;
+		horiz = 0.0f;
+		gameOver = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vert = Input.GetAxis ("Vertical");
+		horiz = Input.GetAxis ("Horizontal");
+		Sprinting ();
+	}
+
+	void FixedUpdate() {
+		if (gameOver) {
+			Application.Quit ();
+		}
+		animator.SetFloat ("player_walk", vert);
+		animator.SetFloat ("player_turn", horiz);
+		animator.SetFloat ("player_sprint", sprint);
+
+		if (transform.position.x > 10 || transform.position.x < -10 || transform.position.z > 10 || transform.position.z < -10)
+			gameOver = true;
+
+
+	}
+
+	void Sprinting() {
+		if (Input.GetButton ("Fire1"))
+			sprint = 1.0f;
+		else 
+			sprint = 0.0f;
+	}
+}
---
+++ PA2/Assets/_Scripts/RobotController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+	Animator animator;
+	public GameObject robotRootObject;
+	static bool active;
+
+	float turn;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		active = false;
+		turn = 0.0f;
+	}
+
+	void Update() {
+	}
+
+	void FixedUpdate () {
+		animator.SetBool ("robot_active", active);
+	}
+
+	public static void setActive() {
+		active = true;
+	}
+
+}
